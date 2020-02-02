4e07ce3d8b339f00e32ed781f2d3fae914b4ab27
Matías Lafroce <mlafroce@gmail.com>
2020-02-02 18:03:47 -0300

    Resolve #6 Adds desktop entry

---
+++ packaging/README.md
@@ -0,0 +1,5 @@
+Render logo with
+
+~~~
+inkscape -z -D -e logo.png -w 128 -h 128 logo.svg
+~~~
--- packaging/create-deb.sh
+++ packaging/create-deb.sh
@@ -2,13 +2,21 @@ read -p 'Package version (ex: 1.0-1): ' VERSION
 
 PACKAGE_NAME="avr-avogadro_${VERSION}"
 
-APP=../target/release/avr-avogadro
-if [ ! -f "$APP" ]; then
-    echo "$APP does not exist, build in parent directory with 'cargo build --release'"
+APP_NAME=avr-avogadro
+BUILT_APP=../target/release/$APP_NAME
+
+if [ ! -f "$BUILT_APP" ]; then
+    echo "$BUILT_APP does not exist, build in parent directory with 'cargo build --release'"
 else 
     cp -r deb $PACKAGE_NAME
-    mkdir -p $PACKAGE_NAME/usr/local/bin/
-    cp -p $APP $PACKAGE_NAME/usr/local/bin/
-    dpkg-deb --build $PACKAGE_NAME
+    sed -i s_\${VERSION}_${VERSION}_g $PACKAGE_NAME/DEBIAN/control
+    mkdir -p $PACKAGE_NAME/usr/bin/
+    mkdir -p $PACKAGE_NAME/usr/share/applications/
+    mkdir -p $PACKAGE_NAME/usr/share/pixmaps/
+    cp deb/$APP_NAME.desktop $PACKAGE_NAME/usr/share/applications/
+    cp logo.png $PACKAGE_NAME/usr/share/pixmaps/$APP_NAME.png
+    objcopy --strip-debug --strip-unneeded $BUILT_APP $PACKAGE_NAME/usr/bin/$APP_NAME
+    chmod -R 755 $PACKAGE_NAME 
+    fakeroot dpkg-deb --build $PACKAGE_NAME
     rm -r $PACKAGE_NAME
 fi
